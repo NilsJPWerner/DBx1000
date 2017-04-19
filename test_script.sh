102b32fd2872afbe7764a488d5032c568d4ad929
Nils Werner <nils.jp.werner@gmail.com>
2017-04-18 22:13:57 -0500

    Renamed test script and fixed float regex

--- test_script.sh
+++ test_script.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 read -p "Enter number of runs: " runs
 runs=${runs:-1} # deafult 1
 
@@ -10,7 +11,7 @@ for i in {0..$runs}; do
     results=$(./rundb)
     txn_count=$(echo $results | grep -P -o "(?<=txn_cnt=)\d+")
     abort_cnt=$(echo $results | grep -P -o "(?<=abort_cnt=)\d+")
-    run_time=$(echo $results | grep -P -o "(?<=run_time=)\d+")
+    run_time=$(echo $results | grep -P -o "(?<=run_time=)\d+\.\d+")
     echo "$i,$txn_count,$abort_cnt,$run_time" >> $dest
 done
 
