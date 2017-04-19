c718229b22ec02f7b3d87e829818cb5bcebf50cf
Nils Werner <nils.jp.werner@gmail.com>
2017-04-18 22:24:51 -0500

    More changes to test_script

--- test_script.sh
+++ test_script.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 read -p "Enter number of runs: " runs
 runs=${runs:-1} # deafult 1
 
@@ -7,12 +7,14 @@ dest=${dest:-'tests/results.csv'}
 
 > $dest # Clean out destination
 echo "run_num,txn_count,aborts,run_time" >> $dest
-for i in {0..$runs}; do
+for i in {0..$runs}
+do
     results=$(./rundb)
     txn_count=$(echo $results | grep -P -o "(?<=txn_cnt=)\d+")
     abort_cnt=$(echo $results | grep -P -o "(?<=abort_cnt=)\d+")
     run_time=$(echo $results | grep -P -o "(?<=run_time=)\d+\.\d+")
     echo "$i,$txn_count,$abort_cnt,$run_time" >> $dest
+    echo "Run $i done."
 done
 
 echo "Done! \nStatistics saved to: $dest"
