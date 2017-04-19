624584164ef2384bf9cca951d6b8908ff3cdd4b0
Nils Werner <nils.jp.werner@gmail.com>
2017-04-19 00:28:05 -0500

    Cut down temp stat structure

--- utilities/temp_stats.cpp
+++ utilities/temp_stats.cpp
@@ -4,67 +4,39 @@
 #include "global.h"
 #include "helper.h"
 
-PageTemperature::PageTemperature() {
-    temperature = 0;
-}
-
-short
-PageTemperature::get_temperature() {
-    return temperature;
-}
-
-void
-PageTemperature::increase_temperature() {
-    // add locks in a bit
-    srand(time(0));
-    double probability = pow (2.0, -1.0 * temperature);
-	bool increment = (rand() / (double) RAND_MAX) < probability;
-    if (increment) {
-        temperature++;
-    }
-}
-
-
 PageTemperatures::PageTemperatures() {
     page_temps = {};
     lock = new pthread_mutex_t;
-    // pthread_mutex_init(lock, NULL);
 }
 
 void
 PageTemperatures::add_temperature(uint64_t row_addr) {
     // Locks to prevent write contention on simultaneous create same page temp
     pthread_mutex_lock(lock);
-    unsigned long page = address_to_page(row_addr);
+    unsigned long page = row_addr / PAGE_SIZE;
     if (!page_temps[page]) {
-        page_temps[page] = new PageTemperature;
+        page_temps[page] = 0;
     }
     pthread_mutex_unlock(lock);
 }
 
-short
+unsigned short
 PageTemperatures::get_temperature(uint64_t row_addr) {
-    PageTemperature* temp = address_to_temp(row_addr);
-    if (!temp) cout << "HASHING FAILURE";
-    return temp->get_temperature();
+    return page_temps[row_addr / PAGE_SIZE];
 }
 
 void
 PageTemperatures::increase_temperature(uint64_t row_addr) {
-    address_to_temp(row_addr)->increase_temperature();
+    srand(time(0));
+    unsigned short temp = page_temps[row_addr / PAGE_SIZE];
+    double probability = pow (2.0, -1.0 * temp);
+	bool increment = (rand() / (double) RAND_MAX) < probability;
+    if (increment) {
+        page_temps[row_addr / PAGE_SIZE]++;
+    }
 }
 
 unsigned long
 PageTemperatures::num_active_pages() {
     return page_temps.size();
-}
-
-PageTemperature*
-PageTemperatures::address_to_temp(uint64_t row_addr) {
-    return page_temps[address_to_page(row_addr)];
-}
-
-unsigned long
-PageTemperatures::address_to_page(uint64_t row_addr) {
-    return row_addr / PAGE_SIZE;
 }
--- utilities/temp_stats.h
+++ utilities/temp_stats.h
@@ -5,26 +5,15 @@
 #include "helper.h"
 #include <unordered_map>
 
-class PageTemperature {
-public:
-    PageTemperature();
-    short get_temperature();
-    void increase_temperature();
-private:
-    short temperature;
-};
-
 class PageTemperatures {
 public:
     PageTemperatures();
     void add_temperature(uint64_t row_addr);
-    short get_temperature(uint64_t row_addr);
+    unsigned short get_temperature(uint64_t row_addr);
     void increase_temperature(uint64_t row_addr);
     unsigned long num_active_pages();
 private:
-    PageTemperature* address_to_temp(uint64_t row_addr);
-    unsigned long address_to_page(uint64_t row_addr);
-    std::unordered_map<unsigned long, PageTemperature*> page_temps;
+    std::unordered_map<unsigned long, unsigned short> page_temps;
     pthread_mutex_t * lock;
 };
 
