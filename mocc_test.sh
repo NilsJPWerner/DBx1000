c4cc3f00dccc08962821120c08377c587b2c1605
Nils Werner <nils.jp.werner@gmail.com>
2017-04-18 19:25:45 -0500

    New test script

--- mocc_test.sh
+++ mocc_test.sh
@@ -1,43 +1,15 @@
-echo "setup,thread_cnt,txn_count,aborts,run_time" >>  tests/results.csv
+read -p "Enter number of runs: " runs
+runs=${runs:-1} # deafult 1
 
-cp config.h temp
-cp tests/config_plain_silo.h config.h
+read -p "Enter destination file (default: 'tests/results.csv'): " dest
+dest=${dest:-'tests/results.csv'}
 
-make -j
-for i in {0..5}; do
-    results="$(./rundb)"
-    thread_cnt=$(echo $results | grep -E -o 'Thread Count: [0-9]{1,4}' | sed 's/Thread Count: //')
-    txn_count=$(echo $results | grep -E -o '\[summary\] txn_cnt=[0-9]{3,6}' | sed 's/\[summary\] txn_cnt=//')
-    aborts=$(echo $results | grep -E -o 'abort_cnt=[0-9]{1,6}' | sed 's/abort_cnt=//')
-    run_time=$(echo $results | grep -E -o 'run_time=[0-9.]{3,10}' | sed 's/run_time=//')
-    echo "silo,$thread_cnt,$txn_count,$aborts,$run_time" >> tests/results.csv
+> $dest # Clean out destination
+echo "run_num,txn_count,aborts,run_time" >> dest
+for i in {0..$runs}; do
+    results=$(./rundb)
+    txn_count=$(echo $results | grep -P -o "(?<=txn_cnt=)\d+")
+    abort_cnt=$(echo $results | grep -P -o "(?<=abort_cnt=)\d+")
+    run_time=$(echo $results | grep -P -o "(?<=run_time=)\d+")
+    echo "$i,$txn_count,$abort_cnt,$run_time" >> dest
 done
-
-rm config.h
-cp tests/config_plain_silo_atomic.h config.h
-
-make -j
-for i in {0..5}; do
-    results="$(./rundb)"
-    thread_cnt=$(echo $results | grep -E -o 'Thread Count: [0-9]{1,4}' | sed 's/Thread Count: //')
-    txn_count=$(echo $results | grep -E -o '\[summary\] txn_cnt=[0-9]{3,6}' | sed 's/\[summary\] txn_cnt=//')
-    aborts=$(echo $results | grep -E -o 'abort_cnt=[0-9]{1,6}' | sed 's/abort_cnt=//')
-    run_time=$(echo $results | grep -E -o 'run_time=[0-9.]{3,10}' | sed 's/run_time=//')
-    echo "silo_atomic,$thread_cnt,$txn_count,$aborts,$run_time" >> tests/results.csv
-done
-
-rm config.h
-cp tests/config_record_temp.h config.h
-
-make -j
-for i in {0..5}; do
-    results="$(./rundb)"
-    thread_cnt=$(echo $results | grep -E -o 'Thread Count: [0-9]{1,4}' | sed 's/Thread Count: //')
-    txn_count=$(echo $results | grep -E -o '\[summary\] txn_cnt=[0-9]{3,6}' | sed 's/\[summary\] txn_cnt=//')
-    aborts=$(echo $results | grep -E -o 'abort_cnt=[0-9]{1,6}' | sed 's/abort_cnt=//')
-    run_time=$(echo $results | grep -E -o 'run_time=[0-9.]{3,10}' | sed 's/run_time=//')
-    echo "record_temp,$thread_cnt,$txn_count,$aborts,$run_time" >> tests/results.csv
-done
-
-rm config.h
-mv temp config.h
